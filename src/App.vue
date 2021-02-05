bd092a0e83bf76dfc6322b9a8a42b91d674a6c21
Glowrare <aajibolanle@yahoo.com>
2021-02-05 01:33:30 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <the-header></the-header>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <div class="logo-title">
+      <img src="../assets/logo.png" alt="Hero Finder logo">
+      <h1>Hero Finder</h1>
+    </div>
+    <div class="search-box">
+      <input type="text" name="search" id="search" placeholder="Type a superhero name here" v-model="searchHero">
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchHero: ""
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
